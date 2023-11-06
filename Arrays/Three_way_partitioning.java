6f4def3c6022361f4b6e99c8e9a3e32f1dde46f2
VENKATA RAMANA KONDAPALLI <heyykvr@gmail.com>
2023-11-06 22:44:19 +0530

    Create Three_way_partitioning.java

---
+++ Arrays/Three_way_partitioning.java
@@ -0,0 +1,40 @@
+// Given an array of size n and a range [a, b]. The task is to partition the array around the range such that array is divided into three parts.
+// 1) All elements smaller than a come first.
+// 2) All elements in range a to b come next.
+// 3) All elements greater than b appear in the end.
+// The individual elements of three sets can appear in any order. You are required to return the modified array.
+
+// Note: The generated output is 1 if you modify the given array successfully.
+
+
+
+
+
+
+class Solution{
+    //Function to partition the array around the range such 
+    public static void threeWayPartition(int array[], int a, int b) {
+        int n = array.length;
+        int start = 0, end = n - 1;
+        int i = 0;
+
+        while (i <= end) {
+            if (array[i] < a) {
+                swap(array, i, start);
+                i++;
+                start++;
+            } else if (array[i] > b) {
+                swap(array, i, end);
+                end--;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
